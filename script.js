945483d7e8162950c7ba79625ea66d12448c7766
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2024-02-14 12:36:07 -0600

    center nav

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const copy = {
-  bigAlCopy: "<p>Hey! I'm Alex Berger, aka Big Al. I'm a Certified Personal Trainer in Austin, TX. I grew up in St. Petersburg, Florida, playing tennis, riding my bike, and rollerblading. My last year of college I learned to lift weights and quickly became addicted to it! At the same time, I started a yoga and handstand practice. When I moved to Austin after graduation, I joined Hyde Park Gym. I discovered and fell in love with powerlifting. The more I moved, the better I felt, and as a goal-oriented person, I loved realizing hard-won progress. Most days, you can find me moving my body in some way! A few of my favorite ways to move are strength training, yoga and dancing. I want to share my passion for exercise and help you reach your strength, skill, health or physique goals. Whether you've never stepped foot in a gym or you're a seasoned gym rat, it's my ambition to empower you to move through life with confidence and ease.</p>",
+  bigAlCopy: "<p>Hey! I'm Alex Berger, aka Big Al. I'm a Certified Personal Trainer in Austin, TX. I grew up in St. Petersburg, Florida, playing tennis, riding my bike, and rollerblading. My last year of college I learned to lift weights and quickly became addicted to it! At the same time, I started a yoga and handstand practice. When I moved to Austin after graduation, I joined Hyde Park Gym. I discovered and fell in love with powerlifting. The more I moved, the better I felt, and as a goal-oriented person, I loved realizing hard-won progress.</p><p> Most days, you can find me moving my body in some way! A few of my favorite ways to move are strength training, yoga and dancing. I want to share my passion for exercise and help you reach your strength, skill, health or physique goals. Whether you've never stepped foot in a gym or you're a seasoned gym rat, it's my ambition to empower you to move through life with confidence and ease.</p><p>Want to see if we're a good fit? Call or text me at 727-504-4123 or email me at aberger3647@gmail.com to set up a free consultation.</p>",
   trainingCopy: `<p>
   Strength Training. General strength training—get stronger in and out
   of the gym. Build muscle, lose fat, and build endurance.
@@ -19,7 +19,7 @@ const copy = {
   handstand? Get upside down with me! Already know how to handstand?
   We can focus on skills like handstand press and pushup.
   </p>`,
-  testimonialsCopy: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id nunc nibh. Quisque efficitur vitae lectus quis porta. In eu tortor et lacus pretium gravida. Mauris efficitur vehicula maximus. Sed felis arcu, consectetur eget orci non, convallis pellentesque orci. Nulla at hendrerit quam. Morbi accumsan ligula mauris, sit amet vulputate felis luctus non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin at egestas</p>",
+  testimonialsCopy: '<p>"Alex is great, she&#39s super kind, patient, and knowledgeable." - Steven G</p><p>Steven went from deadlifting 185 lb to repping his old 1RM for 12!</p><p>"Big Al works in all barbell arenas, weight lifting, body building and power lifting.  If you don&#39t know the difference, that&#39s her specialty.  She&#39s extremely knowledgable and great to work with, which comes in handy during the rest times.  I highly recommend having a consult with Big Al and see what you think. Make it a habit!" - Hill M</p><p>Hill went from using 125 lb of assistance on dips to 35!</p><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at lorem leo. Mauris vestibulum, nunc eu maximus egestas, libero felis aliquet ipsum, vitae tempor massa nunc sagittis ex." -Alexis G</p><p>Alexis added 30 lbs to her deadlift in 4 months!</p>',
   servicesCopy: `<ul>
 <li>
   <p>
@@ -43,7 +43,7 @@ const copy = {
   </p>
 </li>
 </ul>`,
-contactCopy:  "<p>Contact stuff</p>"
+contactCopy:  "<p>Call or text me at 727-504-4123, or shoot me an email at aberger3647@gmail.com.</p>"
 }
 
 const content = [
